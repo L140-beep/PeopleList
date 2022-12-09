0604d9bda9a68e72793322be1e79a8c23e70b0fa
Ivan Petr <ivan.kiel@gmail.com>
2022-12-09 09:29:01 +0800

    TODO added

--- app/src/main/java/ru/myitschool/peoplelistk/MainActivity.kt
+++ app/src/main/java/ru/myitschool/peoplelistk/MainActivity.kt
@@ -13,11 +13,16 @@ class MainActivity : AppCompatActivity() {
         val lvPeople = findViewById<ListView>(R.id.people)
         val people = arrayOf("Petya", "Vasya", "Mary") // массив неизменяем
         // TODO: сгенерировать список персон из случайных сочетаний имён и фамилий
+        // TODO: создайте два string-array в ресурсах и получите список их случайных комбинаций
         val adapter = ArrayAdapter<String>(this, R.layout.item, people)
         lvPeople.adapter = adapter // задаём адаптер (посредник) для отображения данных на списке
+
+        // пример чтения строк из ресурсов
+        val sampleList = resources.getStringArray(R.array.samplelist) // функция возвращает массив
     }
 
     fun onAddPersonClick(view: View) {
+
         // TODO: реализовать добавление новых персон в список
         // имя считывать из текстового поля
         // если нужно изменять число элементов, используйте MutableList<String>
