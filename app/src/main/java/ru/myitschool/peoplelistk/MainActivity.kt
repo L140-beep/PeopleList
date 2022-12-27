960be8bc64588d362df0cf2555ed300790b545d6
L140-beep <kartoshka0302@mail.ru>
2022-12-27 13:40:54 +0800

    complete task

--- app/src/main/java/ru/myitschool/peoplelistk/MainActivity.kt
+++ app/src/main/java/ru/myitschool/peoplelistk/MainActivity.kt
@@ -4,27 +4,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var surnames : Array<String>
+    lateinit var names : Array<String>
+    val people = mutableListOf<String>()
+    lateinit var lvPeople : ListView
+    lateinit var adapter : ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lvPeople = findViewById<ListView>(R.id.people)
-        val people = arrayOf("Petya", "Vasya", "Mary") // массив неизменяем
+        lvPeople = findViewById<ListView>(R.id.people)
         // TODO: сгенерировать список персон из случайных сочетаний имён и фамилий
         // TODO: создайте два string-array в ресурсах и получите список их случайных комбинаций
-        val adapter = ArrayAdapter<String>(this, R.layout.item, people)
-        lvPeople.adapter = adapter // задаём адаптер (посредник) для отображения данных на списке
-
-        // пример чтения строк из ресурсов
-        val sampleList = resources.getStringArray(R.array.samplelist) // функция возвращает массив
+        surnames = resources.getStringArray(R.array.surname)
+        names = resources.getStringArray(R.array.name)
+        adapter = ArrayAdapter<String>(this, R.layout.item)
+        adapter.notifyDataSetChanged()
+        lvPeople.adapter = adapter // задаём адаптер (посредник) для отображения данных на списка
+        findViewById<Button>(R.id.main_btn).setOnClickListener { onAddPersonClick() }
     }
 
-    fun onAddPersonClick(view: View) {
+    fun onAddPersonClick() {
+        var name = findViewById<EditText>(R.id.new_name).text
+        var surname = surnames[Random.nextInt(surnames.size)]
+        var new_name = names[Random.nextInt(names.size)]
+        if (name.toString() != ""){
+            adapter.add(name.toString() + " " + surname)
+        }
+        else{
+            adapter.add(new_name + " " + surname)
+        }
 
-        // TODO: реализовать добавление новых персон в список
-        // имя считывать из текстового поля
-        // если нужно изменять число элементов, используйте MutableList<String>
+        adapter.notifyDataSetChanged()
+
     }
 }
